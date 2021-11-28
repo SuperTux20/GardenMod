a5c35f2fc4fbb0603661a6ac703af8d366c0a014
Tux Penguin <tuxpengwin@gmail.com>
2021-11-28 09:55:27 -0800

    condense

--- src/main/java/net/gardenmod/RawEmeraldBlock.java
+++ src/main/java/net/gardenmod/RawEmeraldBlock.java
@@ -5,8 +5,4 @@ import net.minecraft.block.Material;
 import net.minecraft.sound.BlockSoundGroup;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 
-public class RawEmeraldBlock extends AmethystBlock {
-	public RawEmeraldBlock() {
-		super(FabricBlockSettings.of(Material.AMETHYST).sounds(BlockSoundGroup.AMETHYST_BLOCK).strength(1.5f, 1.5f));
-	}
-}
+public class RawEmeraldBlock extends AmethystBlock { public RawEmeraldBlock() { super(FabricBlockSettings.of(Material.AMETHYST).sounds(BlockSoundGroup.AMETHYST_BLOCK).strength(1.5f, 1.5f)); } }
