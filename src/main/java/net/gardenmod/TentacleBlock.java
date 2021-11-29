aa69d940dc7f6a5c5c6aac8b50e9f575218ba92b
Tux Penguin <tuxpengwin@gmail.com>
2021-11-28 17:14:48 -0800

    condense

--- src/main/java/net/gardenmod/TentacleBlock.java
+++ src/main/java/net/gardenmod/TentacleBlock.java
@@ -5,8 +5,4 @@ import net.minecraft.block.Material;
 import net.minecraft.sound.BlockSoundGroup;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 
-public class TentacleBlock extends Block {
-	public TentacleBlock() {
-		super(FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WET_GRASS).strength(2, 0.5F));
-	}
-}
+public class TentacleBlock extends Block { public TentacleBlock() { super(FabricBlockSettings.of(Material.WOOL).sounds(BlockSoundGroup.WET_GRASS).strength(2, 0.5F)); } }
