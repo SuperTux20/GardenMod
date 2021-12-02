989037c1cb45d9d522d0bcd94fc43248f90dd918
Tux Penguin <tuxpengwin@gmail.com>
2021-12-02 12:42:42 -0800

    rename

--- src/main/java/net/gardenmod/Tags.java
+++ src/main/java/net/gardenmod/Tags.java
@@ -5,7 +5,7 @@ import net.minecraft.block.Block;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 
-public class BlockTags {
-	public static final Tag<Block> MYTHRIL_ORES = TagFactory.BLOCK.create(new Identifier("c", "mythril_ores"));
+public class Tags {
+	public static final Tag<Block> MYTHRIL_ORES    = TagFactory.BLOCK.create(new Identifier("c", "mythril_ores"   ));
 	public static final Tag<Block> ORICHALCUM_ORES = TagFactory.BLOCK.create(new Identifier("c", "orichalcum_ores"));
 }
