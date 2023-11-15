6bdd66f4c6798dffb97b937d9698b54376e76dc1
Tux Penguin <tuxpengwin@gmail.com>
2023-11-14 16:22:49 -0800

    remove regen from cinnamon roll

--- src/main/java/com/supertux20/gardenmod/items/CinnamonRoll.java
+++ src/main/java/com/supertux20/gardenmod/items/CinnamonRoll.java
@@ -2,16 +2,15 @@ package com.supertux20.gardenmod.items;
 
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 
 public class CinnamonRoll extends Item {
 	public CinnamonRoll() {
-		super(new QuiltItemSettings()
-				.food(new FoodComponent.Builder().hunger(2).saturationModifier(3f)
-						.statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 20 * 10), 1).alwaysEdible()
-						.build()));
+		super(new QuiltItemSettings().food(
+			new FoodComponent.Builder()
+			.hunger(2)
+			.saturationModifier(2)
+			.build()));
 	}
 }
