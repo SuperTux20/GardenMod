595ba895a2f2435bdf956a3c14f6768f3acb1d7a
Tux Penguin <tuxpengwin@gmail.com>
2023-11-14 16:22:17 -0800

    change chocolate gravel to gravelblock instead of fallingblock

--- src/main/java/com/supertux20/gardenmod/blocks/ChocolateGravel.java
+++ src/main/java/com/supertux20/gardenmod/blocks/ChocolateGravel.java
@@ -1,10 +1,11 @@
 package com.supertux20.gardenmod.blocks;
 
 import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
-import net.minecraft.block.FallingBlock;
+
+import net.minecraft.block.GravelBlock;
 import net.minecraft.sound.BlockSoundGroup;
 
-public class ChocolateGravel extends FallingBlock {
+public class ChocolateGravel extends GravelBlock {
 	public ChocolateGravel() {
 		super(QuiltBlockSettings.create().sounds(BlockSoundGroup.GRAVEL).strength(0.5F, 0.5F));
 	}
