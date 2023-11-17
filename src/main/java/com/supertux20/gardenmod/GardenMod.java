f961c45e619f4a234aa7b8d5aae18a148a3565e0
Tux Penguin <tuxpengwin@gmail.com>
2023-11-16 18:07:44 -0800

    shortened logger

--- src/main/java/com/supertux20/gardenmod/GardenMod.java
+++ src/main/java/com/supertux20/gardenmod/GardenMod.java
@@ -12,7 +12,7 @@ public class GardenMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Jewel's Garden Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("Jewel's Garden");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
