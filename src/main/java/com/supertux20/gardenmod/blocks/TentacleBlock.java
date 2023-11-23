1f91b93199e9520e165cd961c356d1a5f9af0fca
Tux Penguin <tuxpengwin@gmail.com>
2023-11-22 21:54:59 -0800

    figure out how slowdown works

--- src/main/java/com/supertux20/gardenmod/blocks/TentacleBlock.java
+++ src/main/java/com/supertux20/gardenmod/blocks/TentacleBlock.java
@@ -1,11 +1,11 @@
 package com.supertux20.gardenmod.blocks;
 
 import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
-import net.minecraft.block.Block;
+import net.minecraft.block.SoulSandBlock;
 import net.minecraft.sound.BlockSoundGroup;
 
-public class TentacleBlock extends Block {
+public class TentacleBlock extends SoulSandBlock {
 	public TentacleBlock() {
-		super(QuiltBlockSettings.create().sounds(BlockSoundGroup.WET_GRASS).strength(1F, 1F));
+		super(QuiltBlockSettings.create().sounds(BlockSoundGroup.WET_GRASS).strength(1F, 1F).velocityMultiplier(.6F));
 	}
 }
